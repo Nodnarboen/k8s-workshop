0eb42933af57f9f5d16220920a6793dbbda2d37e
Brandon Neo <brandon.neo@gmail.com>
2019-03-11 00:11:08 +1100

    Update recycle-sockshop-frontend-production.sh

--- recycle-sockshop-frontend-production.sh
+++ recycle-sockshop-frontend-production.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 kubectl scale --replicas=0 --current-replicas=1 deployments -l tier=frontend -n production
-
-sleep 5s
-
-kubectl get deployments -n production
-
+sleep 10s
+kubectl get deployments -n production -l tier=frontend
 kubectl scale --replicas=1 --current-replicas=0 deployments -l tier=frontend -n production
-
-sleep 5s
-
-kubectl get deployments -n production
+sleep 10s
+kubectl get deployments -n production -l tier=frontend
