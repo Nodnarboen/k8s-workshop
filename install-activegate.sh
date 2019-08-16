357c341c5b005507be7398b91b07c53bbdce860e
nodnarboen <brandon.neo@gmail.com>
2019-08-17 04:19:00 +1000

    v3

--- install-activegate.sh
+++ install-activegate.sh
@@ -3,7 +3,9 @@
 sudo wget -O Dynatrace-ActiveGate-Linux-x86-1.173.298.sh "https://jlp305.dynatrace-managed.com/e/$1-k8s-performgo-2019/api/v1/deployment/installer/gateway/unix/latest?Api-Token=$2&arch=x86&flavor=default" 
 sudo /bin/sh Dynatrace-ActiveGate-Linux-x86-1.173.298.sh 
 
-sudo cat /var/lib/dynatrace/gateway/config/custom.properties <<EOF
+sleep 10s
+
+sudo cat <<EOF >/var/lib/dynatrace/gateway/config/custom.properties
 [http.client.external]
 hostname-verification = no
 certificate-validation = no
