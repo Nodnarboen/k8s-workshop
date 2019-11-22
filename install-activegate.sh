f867271398c81c1fea56c1d1ec2c088ea9daee3d
Brandon Neo <brandon.neo@gmail.com>
2019-11-22 23:27:22 +1100

    Update install-activegate.sh

--- install-activegate.sh
+++ install-activegate.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-sudo wget -O Dynatrace-OneAgent-Linux-1.177.220.sh "https://mou612.managed-sprint.dynalabs.io/e/dynatrace-k8s-$1/api/v1/deployment/installer/gateway/unix/latest?Api-Token=$2&arch=x86&flavor=default" 
-sudo /bin/sh Dynatrace-OneAgent-Linux-1.177.220.sh 
+command=$(curl -X GET "https://$1.live.dynatrace.com/api/v1/deployment/installer/gateway/unix/latest" -H "accept: application/octet-stream" -H "Authorization: Api-Token $2")
+echo "Downloading Activegate"
+sleep 30s
 
-sleep 10s
 
-sudo chmod 777 -Rf /var/lib/dynatrace/gateway/config/
+#sudo wget -O Dynatrace-OneAgent-Linux-1.177.220.sh "https://mou612.managed-sprint.dynalabs.io/e/dynatrace-k8s-$1/api/v1/deployment/installer/gateway/unix/latest?Api-Token=$2&arch=x86&flavor=default" 
+#sudo /bin/sh Dynatrace-OneAgent-Linux-1.177.220.sh 
 
-sudo cat <<EOF > /var/lib/dynatrace/gateway/config/custom.properties
-[http.client.external]
-hostname-verification = no
-certificate-validation = no
-EOF
+#sleep 10s
 
-sudo service dynatracegateway stop 
-sleep 10s
-sudo service dynatracegateway start
+#sudo chmod 777 -Rf /var/lib/dynatrace/gateway/config/
+
+#sudo cat <<EOF > /var/lib/dynatrace/gateway/config/custom.properties
+#[http.client.external]
+#hostname-verification = no
+#certificate-validation = no
+#EOF
+
+#sudo service dynatracegateway stop 
+#sleep 10s
+#sudo service dynatracegateway start
