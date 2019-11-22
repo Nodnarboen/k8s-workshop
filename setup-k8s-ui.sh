1da3997d2564d47711c61a06e383722ec5f7fbd5
nodnarboen <brandon.neo@gmail.com>
2019-11-23 00:55:44 +1100

    updated

--- setup-k8s-ui.sh
+++ setup-k8s-ui.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+source var.sh
 
-kubectl apply -f kubernetes-monitoring-service-account.yaml
+#kubectl apply -f kubernetes-monitoring-service-account.yaml
 
 printf "Your k8s API URL\n"
-kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' && printf "\n"
+url=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+echo $url 
 printf "Your Bearer token\n"
-kubectl get secret $(kubectl get sa dynatrace-monitoring -o jsonpath='{.secrets[0].name}' -n dynatrace) -o jsonpath='{.data.token}' -n dynatrace | base64 --decode && printf "\n"
+sa=$(kubectl get sa dynatrace-monitoring -o jsonpath='{.secrets[0].name}' -n dynatrace)
+bearertoken=$(kubectl get secret $sa -o jsonpath='{.data.token}' -n dynatrace | base64 --decode)
+echo $bearertoken
 
+curl -X POST "https://$tenantID.live.dynatrace.com/api/config/v1/kubernetes/credentials" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $apitoken" -H "Content-Type: application/json; charset=utf-8" -d "{ \"label\": \"k8sworkshop\", \"endpointUrl\": \"$url\", \"authToken\": \"$bearertoken\", \"active\": true, \"endpointStatus\": \"ASSIGNED\", \"endpointStatusInfo\": \"\"}"
+echo "Connected to Kubernetes API"
