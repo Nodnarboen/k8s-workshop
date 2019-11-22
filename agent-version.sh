590bbd2c6fcca0fe9277229ce19f91453feee4db
nodnarboen <brandon.neo@gmail.com>
2019-11-22 22:10:18 +1100

    New

--- agent-version.sh
+++ agent-version.sh
@@ -1,2 +1,11 @@
-$version = curl -X GET "https://eez69063.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest/metainfo?flavor=default&arch=all&bitness=all" -H "accept: application/json" -H "Authorization: Api-Token 1GBsj6SHQS-xOKw7sDzQK"
-echo $version
+#!/bin/bash
+
+source var.sh
+
+# get latest agent version
+command=$(curl -X GET "https://$tenantID.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest/metainfo?flavor=default&arch=all&bitness=all" -H "accept: application/json" -H "Authorization: Api-Token $paastoken")
+
+echo "$command" | sed -r 's/[^0-9.]*//g'
+
+
+
