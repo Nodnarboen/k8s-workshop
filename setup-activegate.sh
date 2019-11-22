9a46d306bf2d63963525667f60c3f7f099c49580
nodnarboen <brandon.neo@gmail.com>
2019-11-23 00:56:09 +1100

    updated

--- setup-activegate.sh
+++ setup-activegate.sh
@@ -7,7 +7,13 @@ gcloud compute instances create dynatrace-activegate \
 --image-project ubuntu-os-cloud \
 --zone australia-southeast1-a \
 
+echo "Compute VM created"
+
+sleep 20s
+
 gcloud compute scp ~/HOT-k8s/install-activegate.sh \
       dynatrace-activegate:~/ --zone australia-southeast1-a
+echo "Copied file to Activegate"
 
 gcloud compute ssh --zone australia-southeast1-a dynatrace-activegate --command "./install-activegate.sh $tenantID $paastoken"
+echo "SSH in VM to run file"
