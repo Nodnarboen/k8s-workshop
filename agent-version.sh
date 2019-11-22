8247f89b88459c4201dd408d67c17cbd8f907bc9
Brandon Neo <brandon.neo@gmail.com>
2019-11-22 14:55:25 +1100

    Create agent-version.sh

---
+++ agent-version.sh
@@ -0,0 +1,2 @@
+$version = curl -X GET "https://eez69063.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest/metainfo?flavor=default&arch=all&bitness=all" -H "accept: application/json" -H "Authorization: Api-Token 1GBsj6SHQS-xOKw7sDzQK"
+echo $version
