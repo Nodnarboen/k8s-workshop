c5d1f75d5cf72a98d4253101c7baf1f3de8428ad
nodnarboen <brandon.neo@gmail.com>
2019-11-24 13:39:57 +1100

    update

--- install-oneagent-operator.sh
+++ install-oneagent-operator.sh
@@ -2,11 +2,9 @@
 
 read -p 'Dynatrace SaaS(S) or Dynatrace Managed(M) (S/M)?' env
 
-if [[ $env  =~ ^[Ss]$ ]]
-then
+if [[ $env  =~ ^[Ss]$ ]]; then
 read -p 'Dynatrace Tenant ID (ex. https://<TENANT_ID>.live.dynatrace.com): ' tenantID
-elif [[ $env  =~ ^[Mm]$ ]]
-then
+elif [[ $env  =~ ^[Mm]$ ]]; then
 read -p 'Dynatrace Tenant ID (ex. https://<TENANT_ID>.dynatrace-managed.com): ' tenantID
 read -p 'Dynatrace Environment ID (ex. https://<TENANT_ID>.dynatrace-managed.com/e/<ENVIRONMENT_ID>): ' envID
 fi
@@ -16,27 +14,24 @@ read -p 'Dynatrace PaaS Token: ' paastoken
 
 echo ""
 echo -e "Please confirm all are correct: "
-if [[ $env =~ ^[Ss]$ ]]
-then
+if [[ $env =~ ^[Ss]$ ]]; then
 echo "Your Dynatrace URL is: https://$tenantID.live.dynatrace.com"
-elif [[ $env =~ ^[Mm]$ ]]
-then
+elif [[ $env =~ ^[Mm]$ ]]; then
 echo "Your Dynatrace URL is: https://$tenantID.dynatrace-managed.com/e/$envID"
 fi
+
 echo "Dynatrace API Token: $apitoken"
 echo "Dynatrace PaaS Token: $paastoken"
 read -p "Is this all correct? (y/n) : " -n 1 -r
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-cat <<EOF > var.sh
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+cat <<EOF > k8s-workshop/var.sh
 tenantID=$tenantID
 environmentID=$envID
 apitoken=$apitoken
 paastoken=$paastoken
 EOF
-fi
 
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole cluster-admin \
@@ -48,28 +43,30 @@ kubectl create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent
 
 kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken=$apitoken" --from-literal="paasToken=$paastoken"
 
-if [[ -f "cr.yaml" ]]; then
-    rm -f cr.yaml
+if [[ -f "k8s-workshop/cr.yaml" ]]; then
+    rm -f k8s-workshop/cr.yaml
     echo "Removed cr.yaml"
 fi
 
 LATEST_RELEASE=v0.3.1
-curl -o cr.yaml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/cr.yaml
+curl -o k8s-workshop/cr.yaml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/cr.yaml
 
 # read the yml template from a file and values with the strings 
 
 case $envID in
         '')
         echo "SaaS Deployment"
-        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$tenantID'.live.dynatrace.com\/api/' cr.yaml
+        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$tenantID'.live.dynatrace.com\/api/' k8s-workshop/cr.yaml
         ;;
         *)
         echo "Managed Deployment"
-        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$tenantID'.dynatrace-managed.com\/e\/'$envID'\/api/' cr.yaml
+        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$tenantID'.dynatrace-managed.com\/e\/'$envID'\/api/' k8s-workshop/cr.yaml
         ;;
         ?)
         usage
         ;;
 esac
 
-kubectl create -f cr.yaml
+kubectl create -f k8s-workshop/cr.yaml
+
+fi
