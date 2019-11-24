b16f87d0bf629e1ff83217101c75d22d53688fe2
nodnarboen <brandon.neo@gmail.com>
2019-11-24 13:40:23 +1100

    update

--- setup-activegate.sh
+++ setup-activegate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source var.sh
+source /k8s-workshop/var.sh
 
 gcloud compute instances create dynatrace-activegate \
 --image-family ubuntu-1604-lts \
@@ -11,7 +11,7 @@ echo "Compute VM created"
 
 sleep 20s
 
-gcloud compute scp ~/HOT-k8s/install-activegate.sh \
+gcloud compute scp ~/k8s-workshop/install-activegate.sh \
       dynatrace-activegate:~/ --zone australia-southeast1-a
 echo "Copied file to Activegate"
 
